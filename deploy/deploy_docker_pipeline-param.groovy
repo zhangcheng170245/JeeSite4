34d0f1810be607dfe7c9de466f9c3c4c2d927a22
zhangcheng170245 <170902823@qq.com>
2021-02-07 17:59:31 +0800

    git地址修改

--- deploy/deploy_docker_pipeline-param.groovy
+++ deploy/deploy_docker_pipeline-param.groovy
@@ -16,8 +16,8 @@ pipeline {
 
     stages{
         stage('同步源码') {
-            steps {
-                git url:'git@gitee.com:11547299/jeesite4.git', branch:"$params.branch"
+            steps {    //it@github.com:zhangcheng170245/JeeSite4.git
+                git url:'git@github.com:zhangcheng170245/JeeSite4.git', branch:"$params.branch"
             }
         }
 
